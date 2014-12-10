598ff904d99a5c6f77fe323c73174dd931378706
aentan <hello@aentan.com>
2014-12-10 21:13:55 +0800

    tweaks

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -2,6 +2,13 @@
 
 $(function() {
   
+  $(".navbar a, .btn-cta").on("click", function(e) {
+    e.preventDefault();
+    var t = $(this).attr('href');
+    var ty = $(t).position().top;
+    TweenLite.to(window, ty/2000, {scrollTo:{y:ty}, ease:Power3.easeInOut});
+  });
+  
   $(".bg-holder").parallaxScroll({
     friction: 0.6
   });
